52eafd183cc7d273e4b0f0f92f5011bdfc59d985
Jowoon Lee <jwnle31@gmail.com>
2025-03-02 21:01:25 -0500

    Format records response

--- api/wca.ts
+++ api/wca.ts
@@ -1,6 +1,7 @@
 import wretch from 'wretch';
 import { Record } from './main.js';
 import { getIcon } from './cubing-icons.js';
+import { capitalizeFirstLetter, formatAttemptResult } from './format.js';
 
 export async function getRecords(): Promise<Record[]> {
   const response = await wretch('https://live.worldcubeassociation.org/api')
@@ -50,12 +51,12 @@ export async function getRecords(): Promise<Record[]> {
     );
 
   return response.data.recentRecords
-    .filter((r) => r.tag !== 'NR')
+    .filter((r) => r.result.round.competitionEvent.event.id === '333mbf')
     .map((r) => ({
       id: r.id,
       tag: r.tag as 'WR' | 'CR',
-      type: r.type as 'single' | 'average',
-      time: r.attemptResult / 100,
+      type: capitalizeFirstLetter(r.type) as 'Single' | 'Average',
+      time: formatAttemptResult(r.attemptResult, r.result.round.competitionEvent.event.id),
       // get rid of the chinese part, because the Montserrat font doesn't support it, e.g. Ng Jia Quan (黄佳铨)
       person: r.result.person.name.replace(/\(.*\)/g, '').trim(),
       country: r.result.person.country.name,
@@ -63,3 +64,11 @@ export async function getRecords(): Promise<Record[]> {
       icon: getIcon(r.result.round.competitionEvent.event.id),
     }));
 }
+
+async function test() {
+  const lol = await getRecords();
+  console.log(lol);
+}
+
+test();
+
